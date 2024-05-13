c983f7cf3635b45db730b2d1ae93264de91557d8
varshini058 <162911783+varshini058@users.noreply.github.com>
2024-05-13 14:46:35 +0530

    Update getcveinfo.py db changes

--- getcveinfo.py
+++ getcveinfo.py
@@ -2,7 +2,8 @@ import requests
 import json
 import pandas as pd
 from sqlalchemy import create_engine
-
+import psycopg2 
+import io
 url = "https://services.nvd.nist.gov/rest/json/cves/2.0/?lastModStartDate=2024-05-08T13:00:00.000%2B01:00&lastModEndDate=2024-05-10T13:36:00.000%2B01:00"
 
 payload = {}
@@ -41,6 +42,18 @@ df = pd.DataFrame(idlist)
 
 //code to connect to post gres database and creating cve_summary table
 engine = create_engine('postgresql://username:password@localhost:5432/mydatabase') //sample connection details
-df.to_sql('cve_summary', engine)
+# Drop old table and create new empty table
+df.head(0).to_sql('table_name', engine, if_exists='replace',index=False)
+
+conn = engine.raw_connection()
+cur = conn.cursor()
+output = io.StringIO()
+df.to_csv(output, sep='\t', header=False, index=False)
+output.seek(0)
+contents = output.getvalue()
+cur.copy_from(output, 'table_name', null="") # null values become ''
+conn.commit()
+cur.close()
+conn.close()
 
                     
